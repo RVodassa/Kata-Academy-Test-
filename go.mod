95340a85cab8635c9ae50063be27a6d31621150f
RVodassa <assadov.spb@bk.ru>
2024-06-03 08:32:11 +0300

    create main

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RVodassa/Kata-Academy-Test-
+
+go 1.22.3
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+massage := "hello world"
+
+func main() {
+	fmt.Println(massage)
+}
